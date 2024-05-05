1113035f582321905c42632707c56be4269d0667
Sukma Arbianto <devbianberkah@gmail.com>
2024-05-05 19:34:58 +0700

    first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css:["~/assets/main.css"],
   routeRules: {
     // prerender index route by default
     '/': { prerender: true },
   },
+  app:{
+    head:{
+      title:'Pokedex Nuxt',
+      meta:[
+        {
+          name:'description',
+          content:'belajar nuxt'
+        }
+      ],
+    }
+  },
+  runtimeConfig:{
+    public:{
+      baseUrl:'',
+    }
+  }
 });
