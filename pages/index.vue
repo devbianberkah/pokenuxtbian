5803cc708630c4c1583f5c31babe4ccc585fe7f4
Sukma Arbianto <devbianberkah@gmail.com>
2024-05-05 19:46:32 +0700

    remove list poke

--- pages/index.vue
+++ pages/index.vue
@@ -10,21 +10,12 @@ let pokeDetailList = Array();
 
 const { data:pokemons } = await useFetch(`${runtime.public.baseUrl}pokemon?limit=10`);
 console.log(pokemons);
-pokemonList = pokemons._rawValue.results;
-
-for(let pk of pokemonList){
-  const { data:pokeDetail } = await useFetch(pk.url);
-  console.log(pokeDetail);
-  pokeDetailList.push(pokeDetail._rawValue); 
-}
 </script>
 
 <template>
     <div class="page-content">
         <div class="grid">
-                <div class="column" v-for="(p,index) in pokeDetailList" :key="index">
-                  {{ p.name }}
-                </div>
+                
         </div>
     </div>
 </template>
